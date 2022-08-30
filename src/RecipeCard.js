237fef41786ed3af57156d86856ed0517cafa2a4
Maximilian Alba <albam476@gmail.com>
2022-08-29 21:33:12 -0400

    Add RecipeCard CSS

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import './RecipeCard.css'
+//CSS from https://codepen.io/alexpopovich/pen/weMgMJ
 function RecipeCard({ id , title , image }){
-    
+
     return(
-        <div id = {id}>
+        <div id = {id} className = "card" >
+
+            <div className = "recipeDetails">
             <h1>{title}</h1>
             <img src = {image} alt = {title}></img>
+            <p class = "description">Lorem ipsum dolor</p>
+            </div>
+            <button>Add to Cookbook</button>
+
         </div>
     )
 }
